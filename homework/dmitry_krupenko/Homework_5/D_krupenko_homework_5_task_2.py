3803dca1ea31526ab947259a1cf518531ac95099
D-Krupenko <krupenko116@gmail.com>
2026-02-05 01:12:08 +0400

    d_krupenko_homevor_5_done

---
+++ homework/dmitry_krupenko/Homework_5/D_krupenko_homework_5_task_2.py
@@ -0,0 +1,15 @@
+result1 = 'результат операции: 42'
+result1_value_splitted = result1.split()
+c = result1_value_splitted[-1]
+print(c)
+print(int(c) +10)
+result2 = 'результат операции: 514'
+result2_value_splitted = result2.split()
+f = result1_value_splitted[-1]
+print(f)
+print(int(f) +10)
+result3 = 'результат работы программы: 9'
+result3_value_splitted = result3.split()
+j = result1_value_splitted[-1]
+print(j)
+print(int(j) +10)
