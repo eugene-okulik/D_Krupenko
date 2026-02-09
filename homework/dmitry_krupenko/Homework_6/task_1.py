4b41cebbcf0428cd70fd801e1a0520cc921fbad8
D-Krupenko <krupenko116@gmail.com>
2026-02-09 23:46:51 +0400

    Lint_fix_part_2

--- homework/dmitry_krupenko/Homework_6/task_1.py
+++ homework/dmitry_krupenko/Homework_6/task_1.py
@@ -1,5 +1,7 @@
-text_orgin = ('Etiam tincidunt neque erat, quis molestie enim imperdiet vel. '
-               + 'Integer urna nisl, facilisis vitae semper at, dignissim vitae libero')
+text_orgin = (
+    'Etiam tincidunt neque erat, quis molestie enim imperdiet vel. '
+    'Integer urna nisl, facilisis vitae semper at, dignissim vitae libero'
+)
 words = text_orgin.split(' ')
 fin_text = []
 for word in words:
