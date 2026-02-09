58562c40ef0421722ae67a55bf900d18fde1d178
D-Krupenko <krupenko116@gmail.com>
2026-02-09 20:52:50 +0400

    homework_5_comments_fixed

--- homework/dmitry_krupenko/Homework_5/D_krupenko_homework_5_task_2.py
+++ homework/dmitry_krupenko/Homework_5/D_krupenko_homework_5_task_2.py
@@ -1,15 +1,12 @@
 result1 = 'результат операции: 42'
-result1_value_splitted = result1.split()
-c = result1_value_splitted[-1]
-print(c)
-print(int(c) + 10)
+result_index1 = result1.index(": ") + 2
+a = int(result1[result_index1:])
+print(a + 10)
 result2 = 'результат операции: 514'
-result2_value_splitted = result2.split()
-f = result1_value_splitted[-1]
-print(f)
-print(int(f) + 10)
+result_index2 = result2.index(": ") + 2
+b = int(result2[result_index2:])
+print(b + 10)
 result3 = 'результат работы программы: 9'
-result3_value_splitted = result3.split()
-j = result1_value_splitted[-1]
-print(j)
-print(int(j) + 10)
+result_index3 = result3.index(": ") + 2
+b = int(result3[result_index3:])
+print(b + 10)
