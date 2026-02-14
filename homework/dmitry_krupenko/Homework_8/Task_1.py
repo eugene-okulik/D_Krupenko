05555cb02fe06f45d99e1039fbe8bfc5faa793aa
D-Krupenko <krupenko116@gmail.com>
2026-02-14 19:19:42 +0400

    Lint_fix_part_2

--- homework/dmitry_krupenko/Homework_8/Task_1.py
+++ homework/dmitry_krupenko/Homework_8/Task_1.py
@@ -3,12 +3,12 @@ import random
 
 def emploee_salary():
     salary = [10000, 25000, 600, 400, 890, 33333]
-    random_salary = random.sample(salary,1)
+    random_salary = random.sample(salary, 1)
     random_salary_int = random_salary[0]
     bonus = [True, False]
-    bonus2  = random.choices(bonus,k=1)
+    bonus2 = random.choices(bonus, k=1)
     bonus_boolean = bonus2[0]
-    bonus_value = random.randrange(1,500)
+    bonus_value = random.randrange(1, 500)
     if bonus_boolean is True:
         total_salary = '$' + str(random_salary_int + bonus_value)
     else:
